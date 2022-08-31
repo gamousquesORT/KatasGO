4478a2d981a5b219142c83d433ec6d65b879e670
gamousquesORT <mousques@ort.edu.uy>
2022-08-31 18:49:41 -0300

    eliminated duplicate code in test file

--- GildedRose/gildedrose/gildedrose_test.go
+++ GildedRose/gildedrose/gildedrose_test.go
@@ -6,35 +6,18 @@ import (
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
-func Test_UpdateQuality(t *testing.T) {
-	t.Run("given a regular product sold after sellin update quality twice as fast", func(t *testing.T) {
+func TestUpdateQuality(t *testing.T) {
 
-		var items = []*gildedrose.Item{
-			{"OldProd", 0, 10},
+	checkValues := func(t testing.TB, item *gildedrose.Item, expectedSellin int, expectedQuality int) {
+		t.Helper()
+		if item.Quality != expectedQuality {
+			t.Errorf("Name: Expected %d but got %d ", expectedQuality, item.Quality)
 		}
-
-		gildedrose.UpdateQuality(items)
-		var expected = 8
-
-		if items[0].Quality != expected {
-			t.Errorf("Name: Expected %d but got %d ", expected, items[0].Quality)
+		if item.SellIn != expectedSellin {
+			t.Errorf("Name: Expected %d but got %d ", expectedSellin, item.SellIn)
 		}
+	}
 
-	})
-	t.Run("given a regular product quality should never be negative", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"NegQuality", 0, 0},
-		}
-
-		gildedrose.UpdateQuality(items)
-		var expected = 0
-
-		if items[0].Quality != expected {
-			t.Errorf("Name: Expected %d but got %d ", expected, items[0].Quality)
-		}
-
-	})
 	t.Run("given a regular product quality and sellin should decrease", func(t *testing.T) {
 
 		var items = []*gildedrose.Item{
@@ -44,65 +27,74 @@ func Test_UpdateQuality(t *testing.T) {
 		gildedrose.UpdateQuality(items)
 		var expectedSellin = 0
 		var expectedQuality = 0
+		checkValues(t, items[0], expectedSellin, expectedQuality)
 
-		if items[0].Quality != expectedQuality {
-			t.Errorf("Name: Expected %d but got %d ", expectedQuality, items[0].Quality)
-		}
-		if items[0].SellIn != expectedSellin {
-			t.Errorf("Name: Expected %d but got %d ", expectedSellin, items[0].SellIn)
-		}
 	})
+
+	t.Run("given a regular product quality should never be negative", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"NegQuality", 10, 0},
+		}
+
+		gildedrose.UpdateQuality(items)
+		var expectedSellin = 9
+		var expectedQuality = 0
+		checkValues(t, items[0], expectedSellin, expectedQuality)
+	})
+
+	t.Run("given a regular product sold after sellin update quality twice as fast", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"OldProd", 0, 10},
+		}
+
+		gildedrose.UpdateQuality(items)
+		var expectedSellin = -1
+		var expectedQuality = 8
+
+		checkValues(t, items[0], expectedSellin, expectedQuality)
+
+	})
+
 	t.Run("given a Brie quality increases and sellin decrease", func(t *testing.T) {
 
 		var items = []*gildedrose.Item{
-			{"Aged Brie", 2, 1},
+			{"Aged Brie", 10, 10},
 		}
 
 		gildedrose.UpdateQuality(items)
-		var expectedSellin = 1
-		var expectedQuality = 2
+		var expectedSellin = 9
+		var expectedQuality = 11
+		checkValues(t, items[0], expectedSellin, expectedQuality)
 
-		if items[0].Quality != expectedQuality {
-			t.Errorf("Name: Expected %d but got %d ", expectedQuality, items[0].Quality)
-		}
-		if items[0].SellIn != expectedSellin {
-			t.Errorf("Name: Expected %d but got %d ", expectedSellin, items[0].SellIn)
-		}
 	})
 
-	t.Run("given a product quality should ve more than 50", func(t *testing.T) {
+	t.Run("given a product quality should be more than 50", func(t *testing.T) {
 
 		var items = []*gildedrose.Item{
-			{"Aged Brie", 1, 51},
+			{"Aged Brie", 10, 50},
 		}
 
 		gildedrose.UpdateQuality(items)
-		var expectedSellin = 0
+		var expectedSellin = 9
 		var expectedQuality = 50
+		checkValues(t, items[0], expectedSellin, expectedQuality)
 
-		if items[0].Quality != expectedQuality {
-			t.Errorf("Name: Expected %d but got %d ", expectedQuality, items[0].Quality)
-		}
-		if items[0].SellIn != expectedSellin {
-			t.Errorf("Name: Expected %d but got %d ", expectedSellin, items[0].SellIn)
-		}
 	})
 
 	t.Run("given a Sulfuras never sold and quality intact", func(t *testing.T) {
 
 		var items = []*gildedrose.Item{
-			{"Sulfuras", 1, 15},
+			{"Sulfuras, Hand of Ragnaros", 10, 80},
 		}
 
 		gildedrose.UpdateQuality(items)
-		var expectedSellin = 1
-		var expectedQuality = 15
+		var expectedSellin = 10
+		var expectedQuality = 80
+		checkValues(t, items[0], expectedSellin, expectedQuality)
 
-		if items[0].Quality != expectedQuality {
-			t.Errorf("Name: Expected %d but got %d ", expectedQuality, items[0].Quality)
-		}
-		if items[0].SellIn != expectedSellin {
-			t.Errorf("Name: Expected %d but got %d ", expectedSellin, items[0].SellIn)
-		}
 	})
 }
+
+
