0e7a084e06860d967e32bb11588c409969cbccfc
gamousquesORT <mousques@ort.edu.uy>
2022-09-04 17:02:06 -0300

    added all tests

--- GildedRose/gildedrose/gildedrose_test.go
+++ GildedRose/gildedrose/gildedrose_test.go
@@ -86,15 +86,52 @@ func TestUpdateQuality(t *testing.T) {
 	t.Run("given a Sulfuras never sold and quality intact", func(t *testing.T) {
 
 		var items = []*gildedrose.Item{
-			{"Sulfuras, Hand of Ragnaros", 10, 80},
+			{"Sulfuras, Hand of Ragnaros", 10, 40},
 		}
 
 		gildedrose.UpdateQuality(items)
 		var expectedSellin = 10
-		var expectedQuality = 80
+		var expectedQuality = 40
+		checkValues(t, items[0], expectedSellin, expectedQuality)
+
+	})
+
+	t.Run("given a Backstage passes to a TAFKAL80ETC concert after 10 days", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"Backstage passes to a TAFKAL80ETC concert", 10, 40},
+		}
+
+		gildedrose.UpdateQuality(items)
+		var expectedSellin = 9
+		var expectedQuality = 42
+		checkValues(t, items[0], expectedSellin, expectedQuality)
+
+	})
+
+	t.Run("given a Backstage passes to a TAFKAL80ETC concert after 5 days", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"Backstage passes to a TAFKAL80ETC concert", 5, 40},
+		}
+
+		gildedrose.UpdateQuality(items)
+		var expectedSellin = 4
+		var expectedQuality = 43
+		checkValues(t, items[0], expectedSellin, expectedQuality)
+
+	})
+
+	t.Run("given a Backstage passes to a TAFKAL80ETC concert after 0 days", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"Backstage passes to a TAFKAL80ETC concert", 0, 40},
+		}
+
+		gildedrose.UpdateQuality(items)
+		var expectedSellin = -1
+		var expectedQuality = 0
 		checkValues(t, items[0], expectedSellin, expectedQuality)
 
 	})
 }
-
-
