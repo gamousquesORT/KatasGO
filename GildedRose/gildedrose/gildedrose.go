c2ccdf0d682a8b291b858f299b8e46b5c7a735fb
gamousquesORT <mousques@ort.edu.uy>
2022-09-05 20:28:03 -0300

    extract basic methods and change magic numbers

--- GildedRose/gildedrose/gildedrose.go
+++ GildedRose/gildedrose/gildedrose.go
@@ -5,29 +5,27 @@ type Item struct {
 	SellIn, Quality int
 }
 
+const maxQuality = 50
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].Quality > 0 {
 				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
+					NormalQualityDecrease(items, i)
 				}
 			}
 		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
+			if items[i].Quality < maxQuality {
+				IncrementQuality(items, i)
 				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
+						NormalQualityIncrease(items, i)
 					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
+				}
+				if items[i].SellIn < 6 {
+					NormalQualityIncrease(items, i)
 				}
 			}
 		}
@@ -41,18 +39,30 @@ func UpdateQuality(items []*Item) {
 				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].Quality > 0 {
 						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
+							NormalQualityDecrease(items, i)
 						}
 					}
 				} else {
 					items[i].Quality = items[i].Quality - items[i].Quality
 				}
 			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
+				NormalQualityIncrease(items, i)
 			}
 		}
 	}
 
+}
+
+func NormalQualityIncrease(items []*Item, i int) {
+	if items[i].Quality < maxQuality {
+		IncrementQuality(items, i)
+	}
+}
+
+func IncrementQuality(items []*Item, i int) {
+	items[i].Quality = items[i].Quality + 1
+}
+
+func NormalQualityDecrease(items []*Item, i int) {
+	items[i].Quality = items[i].Quality - 1
 }
